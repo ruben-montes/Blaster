472803fd92e726fd00863a2736c37b2d657b5595
Ruben Montes <ruben.montes.mendoza@gmail.com>
2024-07-05 12:45:36 +0200

    58. Applying Aim Offsets

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -33,6 +33,7 @@ protected:
 	void CrouchButtonPressed();
 	void AimButtonPressed();
 	void AimButtonReleased();
+	void AimOffset(float DeltaTime);
 	
 private:
 	UPROPERTY(VisibleAnywhere, Category = Camera)
@@ -56,9 +57,16 @@ private:
 	UFUNCTION(Server, Reliable)
 	void ServerEquipButtonPressed();
 
+	float AO_Yaw;
+	float AO_Pitch;
+	FRotator StartingAimRotation;
+
 public:
 	void SetOverlappingWeapon(AWeapon* Weapon);
 	bool IsWeaponEquipped();
 	bool IsAiming();
+	FORCEINLINE float GetAO_Yaw() const { return AO_Yaw; }
+	FORCEINLINE float GetAO_Pitch() const { return AO_Pitch; }
+	
 };
 
